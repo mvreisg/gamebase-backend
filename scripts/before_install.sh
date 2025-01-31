5bccabe783ad8d9bddc091242dd7e4f771d47dca
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-01-31 17:11:23 -0300

    [update] updated .env and .env.example - deleted deploy.yml to test something - edited code deploy scripts to copy .env from server - edited mariadb connection class to match the .env file

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo rm -rf /var/www/html/gamebase-backend
 sudo mkdir /var/www/html/gamebase-backend
+sudo cp /home/ubuntu/gamebase-backend/.env /var/www/html/gamebase-backend/.env
--- src/infrastructure/database/MariaDBConnection.php
+++ src/infrastructure/database/MariaDBConnection.php
@@ -7,28 +7,14 @@ class MariaDBConnection
 {
     public static function get(): PDO
     {
-        /**
-         * @var PDO
-         */
-        $pdo = $_SERVER["ENVIRONMENT"] === "development" ?
-            new PDO(
-                $_SERVER["MARIADB_DATABASE_DEVELOPMENT_DSN"],
-                $_SERVER["MARIADB_DATABASE_DEVELOPMENT_USERNAME"],
-                $_SERVER["MARIADB_DATABASE_DEVELOPMENT_PASSWORD"],
-                [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                ]
-            ) :
-            new PDO(
-                $_SERVER["MARIADB_DATABASE_PRODUCTION_DSN"],
-                $_SERVER["MARIADB_DATABASE_PRODUCTION_USERNAME"],
-                $_SERVER["MARIADB_DATABASE_PRODUCTION_PASSWORD"],
-                [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                ]
-            );
-        return $pdo;
+        return new PDO(
+            $_SERVER["DATABASE_DSN"],
+            $_SERVER["DATABASE_USERNAME"],
+            $_SERVER["DATABASE_PASSWORD"],
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]
+        );        
     }
 }
