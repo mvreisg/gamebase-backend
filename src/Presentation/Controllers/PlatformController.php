e1763edf5e0cab32e52b48e2f51b84547b06f52d
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-02-03 14:09:52 -0300

    [update] added platform controller docs

--- src/Presentation/Controllers/PlatformController.php
+++ src/Presentation/Controllers/PlatformController.php
@@ -9,54 +9,89 @@ use Mvreisg\GamebaseBackend\Infrastructure\Http\HttpApplication;
 use Mvreisg\GamebaseBackend\Application\Services\PlatformService;
 use Mvreisg\GamebaseBackend\Domain\Exceptions\EntityInvalidValueException;
 use Mvreisg\GamebaseBackend\Infrastructure\Exceptions\DatabaseDuplicatedEntryException;
+use PDOException;
 
+/**
+ * Platform controller class.
+ */
 class PlatformController
 {
+    /**
+     * @var PlatformService $service The service to be user by this controller.
+     */
     private PlatformService $service;
 
+    /**
+     * Platform controller class constructor.
+     * @param PlatformService $service The service to be user by this controller.
+     * @return void
+     */
     public function __construct(PlatformService $service)
     {
         $this->service = $service;
     }
 
+    /**
+     * Method that handles the HTTP request and response of a Platform insertion.
+     * @param HttpRequest $request The HTTP request object.
+     * @param HttpResponse $response The HTTP response object.
+     */
     public function insert(HttpRequest $request, HttpResponse $response)
     {
         $messages = [];
 
         $body = $request->parseBodyFromJSONString();
-
         $name = $body['name'] ?? null;
-
-        if ($name === null) {
-            $messages[] = "O parâmetro 'name' não foi informado no JSON.";
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[400])->sendJSON();
-            return;
-        }
 
         $platform = null;
         try {
             $platform = $this->service->insert($name);
         } catch (EntityInvalidValueException | DatabaseDuplicatedEntryException $e) {
             $messages[] = $e->getMessage();
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[400])->sendJSON();
+            $response
+                ->appendArray([
+                    'messages' => $messages
+                ])
+                ->status(HttpApplication::STATUS_CODES[400])
+                ->sendJSON();
             return;
-        } catch (Exception $e) {
+        } catch (PDOException | Exception $e) {
             $messages[] = $e->getMessage();
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[500])->sendJSON();
+            $response
+                ->appendArray([
+                    'messages' => $messages
+                ])
+                ->status(HttpApplication::STATUS_CODES[500])
+                ->sendJSON();
             return;
         }
 
         if ($platform == false) {
             $messages[] = 'Ocorreu um erro ao inserir a plataforma. Contate o suporte.';
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[500])->sendJSON();
+            $response
+                ->appendArray([
+                    'messages' => $messages
+                ])
+                ->status(HttpApplication::STATUS_CODES[500])
+                ->sendJSON();
             return;
         }
 
         $messages[] = 'Plataforma incluída com sucesso!';
-        $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[201])->sendJSON();
+        $response
+            ->appendArray([
+                'messages' => $messages
+            ])
+            ->status(HttpApplication::STATUS_CODES[201])
+            ->sendJSON();
     }
 
-    public function edit(HttpRequest $request, HttpResponse $response)
+    /**
+     * Method that handles the HTTP request and response of a Platform update.
+     * @param HttpRequest $request The HTTP request object.
+     * @param HttpResponse $response The HTTP response object.
+     */
+    public function update(HttpRequest $request, HttpResponse $response)
     {
         $messages = [];
 
@@ -66,59 +101,55 @@ class PlatformController
         $platformId = $params['platformId'] ?? null;
         $name = $body['name'] ?? null;
 
-        $hasParameterError = false;
-        if ($platformId === null) {
-            $hasParameterError = true;
-            $messages[] = 'O id da plataforma não foi informado na URL.';
-        }
-
-        if ($name === null) {
-            $hasParameterError = true;
-            $messages[] = "O parâmetro 'name' não foi informado no JSON.";
-        }
-
-        if ($hasParameterError) {
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[400])->sendJSON();
-            return;
-        }
-
-        $isPlatformIdNumeric = is_numeric($platformId);
-        if ($isPlatformIdNumeric === false) {
-            $messages[] = "O parâmetro 'platformId' informado precisa ser um número inteiro.";
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[400])->sendJSON();
-            return;
-        }
-
-        $platformId = intval($platformId);
-        if ($platformId <= 0) {
-            $messages[] = "O parâmetro 'platformId' informado precisa ser un número inteiro maior que zero.";
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[400])->sendJSON();
-            return;
-        }
-
         $wasEditAnSuccess = false;
         try {
-            $wasEditAnSuccess = $this->service->edit($platformId, $name);
+            $platformId = intval($platformId);
+            $wasEditAnSuccess = $this->service->update($platformId, $name);
         } catch (EntityInvalidValueException | DatabaseDuplicatedEntryException $e) {
             $messages[] = $e->getMessage();
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[400])->sendJSON();
+            $response
+                ->appendArray([
+                    'messages' => $messages
+                ])
+                ->status(HttpApplication::STATUS_CODES[400])
+                ->sendJSON();
             return;
-        } catch (Exception $e) {
+        } catch (PDOException | Exception $e) {
             $messages[] = $e->getMessage();
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[500])->sendJSON();
+            $response
+                ->appendArray([
+                    'messages' => $messages
+                ])
+                ->status(HttpApplication::STATUS_CODES[500])
+                ->sendJSON();
             return;
         }
 
         if ($wasEditAnSuccess === false) {
             $messages[] = 'Verifique se o id da plataforma existe no banco de dados.';
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[400])->sendJSON();
+            $response
+                ->appendArray([
+                    'messages' => $messages
+                ])
+                ->status(HttpApplication::STATUS_CODES[400])
+                ->sendJSON();
             return;
         }
 
         $messages[] = 'Plataforma editada com sucesso!';
-        $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[200])->sendJSON();
+        $response
+            ->appendArray([
+                'messages' => $messages
+            ])
+            ->status(HttpApplication::STATUS_CODES[200])
+            ->sendJSON();
     }
 
+    /**
+     * Method that handles the HTTP request and response of a Platform search by its id.
+     * @param HttpRequest $request The HTTP request object.
+     * @param HttpResponse $response The HTTP response object.
+     */
     public function findById(HttpRequest $request, HttpResponse $response)
     {
         $messages = [];
@@ -127,38 +158,29 @@ class PlatformController
         $params = $request->getParams();
         $platformId = $params['platformId'] ?? null;
 
-        if ($platformId === null) {
-            $messages[] = 'O id da plataforma não foi informado na URL.';
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[400])->sendJSON();
-            return;
-        }
-
-        $isPlatformIdNumeric = is_numeric($platformId);
-        if ($isPlatformIdNumeric === false) {
-            $messages[] = 'O id da plataforma precisa ser um número inteiro.';
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[400])->sendJSON();
-            return;
-        }
-
-        $platformId = intval($platformId);
-        if ($platformId <= 0) {
-            $messages[] = 'O id da plataforma precisa ser un número inteiro maior que zero.';
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[400])->sendJSON();
-            return;
-        }
-
         $platform = null;
         try {
+            $platformId = intval($platformId);
             $platform = $this->service->findById($platformId);
-        } catch (Exception $e) {
+        } catch (PDOException | Exception $e) {
             $messages[] = $e->getMessage();
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[500])->sendJSON();
+            $response
+                ->appendArray([
+                    'messages' => $messages
+                ])
+                ->status(HttpApplication::STATUS_CODES[500])
+                ->sendJSON();
             return;
         }
 
         if ($platform == null) {
             $messages[] = 'A plataforma procurada não existe!';
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[404])->sendJSON();
+            $response
+                ->appendArray([
+                    'messages' => $messages
+                ])
+                ->status(HttpApplication::STATUS_CODES[404])
+                ->sendJSON();
             return;
         }
 
@@ -171,9 +193,20 @@ class PlatformController
         ];
 
         $messages[] = 'Plataforma encontrada com sucesso!';
-        $response->appendArray(['messages' => $messages, 'data' => $data])->status(HttpApplication::STATUS_CODES[200])->sendJSON();
+        $response
+            ->appendArray([
+                'messages' => $messages,
+                'data' => $data
+            ])
+            ->status(HttpApplication::STATUS_CODES[200])
+            ->sendJSON();
     }
 
+    /**
+     * Method that handles the HTTP request and response of a retrieval of all Platform.
+     * @param HttpRequest $request The HTTP request object.
+     * @param HttpResponse $response The HTTP response object.
+     */
     public function findAll(HttpRequest $request, HttpResponse $response)
     {
         $messages = [];
@@ -182,16 +215,26 @@ class PlatformController
         $platforms = null;
         try {
             $platforms = $this->service->findAll();
-        } catch (Exception $e) {
+        } catch (PDOException | Exception $e) {
             $messages[] = $e->getMessage();
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[500])->sendJSON();
+            $response
+                ->appendArray([
+                    'messages' => $messages
+                ])
+                ->status(HttpApplication::STATUS_CODES[500])
+                ->sendJSON();
             return;
         }
 
         $numberOfPlatforms = count($platforms);
         if ($numberOfPlatforms === 0) {
             $messages[] = 'A busca foi concluída e nenhuma plataforma foi encontrada.';
-            $response->appendArray(['messages' => $messages])->status(HttpApplication::STATUS_CODES[200])->sendJSON();
+            $response
+                ->appendArray([
+                    'messages' => $messages
+                ])
+                ->status(HttpApplication::STATUS_CODES[200])
+                ->sendJSON();
             return;
         }
 
@@ -206,6 +249,12 @@ class PlatformController
         }
 
         $messages[] = 'Plataformas buscadas com sucesso!';
-        $response->appendArray(['messages' => $messages, 'data' => $data])->status(HttpApplication::STATUS_CODES[200])->sendJSON();
+        $response
+            ->appendArray([
+                'messages' => $messages,
+                'data' => $data
+            ])
+            ->status(HttpApplication::STATUS_CODES[200])
+            ->sendJSON();
     }
 }
