e92fc4644004f867f677a6bbcd9e54fda01d58a8
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-02-03 13:36:23 -0300

    [update] added mariadb platform repository docs

--- src/Infrastructure/Repositories/MariaDBPlatformRepository.php
+++ src/Infrastructure/Repositories/MariaDBPlatformRepository.php
@@ -7,15 +7,31 @@ use PDOException;
 use Mvreisg\GamebaseBackend\Domain\Entities\Platform;
 use Mvreisg\GamebaseBackend\Domain\Repositories\PlatformRepositoryInterface;
 
+/**
+ * MariaDB platform repository class.
+ */
 class MariaDBPlatformRepository implements PlatformRepositoryInterface
 {
+    /**
+     * @var PDO $pdo The database conntection object.
+     */
     private PDO $pdo;
 
+    /**
+     * MariaDB platform repository class constructor.
+     * @param PDO $pdo The database conntection object.
+     */
     public function __construct(PDO $pdo)
     {
         $this->pdo = $pdo;
     }
 
+    /**
+     * Inserts a new Platform into the repository.
+     * @param Platform $platform The Platform to be inserted.
+     * @return Platform A copy of the inserted Platform.
+     * @throws PDOException Throwed in case of database error.
+     */    
     public function insert(Platform $platform): Platform
     {
         try {
@@ -46,7 +62,13 @@ class MariaDBPlatformRepository implements PlatformRepositoryInterface
         }
     }
 
-    public function edit(Platform $platform): bool
+    /**
+     * Updates an existing Platform in the repository.
+     * @param Platform $platform The Platform data to be updated.
+     * @return bool The success flag.
+     * @throws PDOException Throwed in case of database error.
+     */    
+    public function update(Platform $platform): bool
     {
         try {
             $id = $platform->getId();
@@ -66,11 +88,23 @@ class MariaDBPlatformRepository implements PlatformRepositoryInterface
         }
     }
 
+    /**
+     * Deletes an existing Platform in the repository.
+     * @param int $id The id of the register to be deleted.
+     * @return bool The success flag.
+     * @throws PDOException Throwed in case of database error.
+     */
     public function delete(int $id): bool
     {
         return false;
     }
 
+    /**
+     * Finds a Platform in the repository by its id.
+     * @param int $id The id to search for.
+     * @return Platform|null Returns the Platform if found, else returns null.
+     * @throws PDOException Throwed in case of database error.
+     */    
     public function findById(int $id): Platform|null
     {
         try {
@@ -91,6 +125,11 @@ class MariaDBPlatformRepository implements PlatformRepositoryInterface
         }
     }
 
+    /**
+     * Finds all Platforms in the repository.
+     * @return array A list containing all the founded repositories.
+     * @throws PDOException Throwed in case of database error.
+     */    
     public function findAll(): array
     {
         try {
@@ -114,6 +153,12 @@ class MariaDBPlatformRepository implements PlatformRepositoryInterface
         }
     }
 
+    /**
+     * Check if the name already exists in the repository.
+     * @param string $name The name to be searched.
+     * @return bool True if it already exists, else returns false.
+     * @throws PDOException Throwed in case of database error.
+     */    
     public function hasDuplicatedNames(string $name): bool
     {
         try {
