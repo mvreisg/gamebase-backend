74dd20d7f692171bc9704a6b534104a325f67b38
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-02-03 14:14:17 -0300

    [update] added platform controller factory docs

--- src/Presentation/Factories/PlatformControllerFactory.php
+++ src/Presentation/Factories/PlatformControllerFactory.php
@@ -7,8 +7,15 @@ use Mvreisg\GamebaseBackend\Infrastructure\Database\MariaDBConnection;
 use Mvreisg\GamebaseBackend\Presentation\Controllers\PlatformController;
 use Mvreisg\GamebaseBackend\Infrastructure\Repositories\MariaDBPlatformRepository;
 
+/**
+ * Platform controller factory class.
+ */
 class PlatformControllerFactory
 {
+    /**
+     * Static method that returns a instance of PlatformController.
+     * @return PlatformController The instance.
+     */
     public static function get(): PlatformController
     {
         $repository = new MariaDBPlatformRepository(MariaDBConnection::get());
