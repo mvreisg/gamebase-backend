d7f77812b2dc4933cb3ea0edbd7a67e2d5a24b53
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-02-06 18:50:36 -0300

    [update] added docs to the game genre entity class

--- src/Domain/Entities/GameGenre.php
+++ src/Domain/Entities/GameGenre.php
@@ -4,12 +4,33 @@ namespace Mvreisg\GamebaseBackend\Domain\Entities;
 
 use Mvreisg\GamebaseBackend\Domain\Exceptions\EntityInvalidValueException;
 
+/**
+ * Game Genre entity class.
+ */
 class GameGenre
 {
+    /**
+     * @var int $id The id of the entity.
+     */
     private int $id;
+
+    /**
+     * @var int $genreId The Genre id of the entity.
+     */
     private int $genreId;
+
+    /**
+     * @var int $gameId The Game id of the entity.
+     */
     private int $gameId;
 
+    /**
+     * Game Genre entity class constructor.
+     * @param int $id [Optional] - The id of the entity.
+     * @param int $genreId [Optional] - The Genre id of the entity.
+     * @param int $gameId [Optional] - The Game id of the entity.
+     * @return void
+     */
     public function __construct(int $id = 0, int $genreId = 0, int $gameId = 0)
     {
         $this->id = $id;
@@ -17,36 +38,68 @@ class GameGenre
         $this->gameId = $gameId;
     }
 
+    /**
+     * Gets the id of the entity.
+     * @return int The id of the entity.
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * Sets the id of the entity.
+     * @param int $id The id of the entity.
+     * @return void
+     */
     public function setId(int $id)
     {
         $this->id = $id;
     }
 
+    /**
+     * Gets the Genre id of the entity.
+     * @return int The Genre id of the entity.
+     */
     public function getGenreId()
     {
         return $this->genreId;
     }
 
+    /**
+     * Sets the Genre id of the entity.
+     * @param int $genreId The Genre id of the entity.
+     * @return void
+     */
     public function setGenreId(int $genreId)
     {
         $this->genreId = $genreId;
     }
 
+    /**
+     * Gets the Game id of the entity.
+     * @return int The Game id of the entity.
+     */
     public function getGameId()
     {
         return $this->gameId;
     }
 
+    /**
+     * Sets the Game id of the entity.
+     * @param int $gameId The Game id of the entity.
+     * @return void
+     */
     public function setGameId(int $gameId)
     {
         $this->gameId = $gameId;
     }
 
+    /**
+     * Validates the id of the entity, throwing an exception if it is invalid.
+     * @throws EntityInvalidValueException Throwed if the id is invalid.
+     * @return void
+     */
     public function validateId()
     {
         if ($this->id < 1) {
@@ -54,6 +107,11 @@ class GameGenre
         }
     }
 
+    /**
+     * Validates the Genre id of the entity, throwing an exception if it is invalid.
+     * @throws EntityInvalidValueException Throwed if the Genre id is invalid.
+     * @return void
+     */
     public function validateGenreId()
     {
         if ($this->genreId < 1) {
@@ -61,6 +119,11 @@ class GameGenre
         }
     }
 
+    /**
+     * Validates the Game id of the entity, throwing an exception if it is invalid.
+     * @throws EntityInvalidValueException Throwed if the Game id is invalid.
+     * @return void
+     */
     public function validateGameId()
     {
         if ($this->gameId < 1) {
