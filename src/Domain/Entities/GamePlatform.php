da01ae7cb45b3e05aecfd18b2018216cb033b2c5
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-02-10 17:16:30 -0300

    [update] game platform entity docs added

--- src/Domain/Entities/GamePlatform.php
+++ src/Domain/Entities/GamePlatform.php
@@ -4,12 +4,33 @@ namespace Mvreisg\GamebaseBackend\Domain\Entities;
 
 use Mvreisg\GamebaseBackend\Domain\Exceptions\EntityInvalidValueException;
 
+/**
+ * Game Platform entity class.
+ */
 class GamePlatform
 {
+    /**
+     * @var int $id The entity id.
+     */
     private int $id;
+
+    /**
+     * @var int $platformId The Platform id.
+     */
     private int $platformId;
+
+    /**
+     * @var int $gameId The Game id.
+     */
     private int $gameId;
 
+    /**
+     * Game Platform entity class constructor.
+     * @param int $id The entity id.
+     * @param int $platformId The Platform id.
+     * @param int $gameId The Game id.
+     * @return void
+     */
     public function __construct(int $id = 0, int $platformId = 0, int $gameId = 0)
     {
         $this->id = $id;
@@ -17,36 +38,68 @@ class GamePlatform
         $this->gameId = $gameId;
     }
 
+    /**
+     * Gets the entity id.
+     * @return int The entity id.
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * Sets the entity id.
+     * @param int $id The entity id.
+     * @return void
+     */
     public function setId(int $id)
     {
         $this->id = $id;
     }
 
+    /**
+     * Gets the Platform id.
+     * @return int The Platform id.
+     */
     public function getPlatformId()
     {
         return $this->platformId;
     }
 
+    /**
+     * Sets the Platform id.
+     * @param int $id The Platform id.
+     * @return void
+     */
     public function setPlatformId(int $platformId)
     {
         $this->platformId = $platformId;
     }
 
+    /**
+     * Gets the Game id.
+     * @return int The Game id.
+     */
     public function getGameId()
     {
         return $this->gameId;
     }
 
+    /**
+     * Sets the Game id.
+     * @param int $id The Game id.
+     * @return void
+     */
     public function setGameId(int $gameId)
     {
         $this->gameId = $gameId;
     }
 
+    /**
+     * Method that validates the entity id, throwing an exception if it is not valid.
+     * @throws EntityInvalidValueException Throwed if the id is not valid.
+     * @return void
+     */
     public function validateId()
     {
         if ($this->id < 1) {
@@ -54,6 +107,11 @@ class GamePlatform
         }
     }
 
+    /**
+     * Method that validates the Platform id, throwing an exception if it is not valid.
+     * @throws EntityInvalidValueException Throwed if the id is not valid.
+     * @return void
+     */
     public function validatePlatformId()
     {
         if ($this->platformId < 1) {
@@ -61,6 +119,11 @@ class GamePlatform
         }
     }
 
+    /**
+     * Method that validates the Game id, throwing an exception if it is not valid.
+     * @throws EntityInvalidValueException Throwed if the id is not valid.
+     * @return void
+     */
     public function validateGameId()
     {
         if ($this->gameId < 1) {
