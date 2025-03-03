53b3619864bf6a6d6df8b402503abe416d00c42b
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-03-03 18:30:41 -0300

    [trying] to fix deploy

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,1 @@
 #!/bin/bash
-sudo docker compose build
-sudo docker compose up
-sudo docker exec -it gamebase-backend php vendor/bin/phinx migrate
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo docker compose down
 sudo rm -rf /var/www/html
 sudo mkdir /var/www/html
 sudo cp /home/ubuntu/.env /var/www/html/.env
