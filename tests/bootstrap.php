ac54d5dfdafa267ba785943e3695512633f2f861
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-03-06 17:49:09 -0300

    [trying] to fix pipeline

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,12 @@
 namespace Mvreisg\GamebaseBackend;
 
 use Dotenv\Dotenv;
+use Throwable;
 
-include_once dirname(__DIR__) . '/vendor/autoload.php';
+try{
+    include_once dirname(__DIR__) . '/vendor/autoload.php';
 
-Dotenv::createImmutable(dirname(__DIR__))->load();
+    Dotenv::createImmutable(dirname(__DIR__))->load();
+} catch (Throwable $e){
+    print_r('Ocorreu um erro! Código ' . $e->getCode());
+}
