e01ccf491f2fe1e0f63740b52a20640245fa6651
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-04-06 11:11:43 -0300

    [update] added permission repository interface

---
+++ src/Domain/Repositories/PermissionRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mvreisg\GamebaseBackend\Domain\Repositories;
+
+use Mvreisg\GamebaseBackend\Domain\Entities\Permission;
+
+interface PermissionRepositoryInterface
+{
+    public function insert(Permission $permission): Permission;
+
+    public function update(Permission $permission): bool;
+
+    public function findById(int $id): Permission|null;
+
+    public function findAll(): array;
+
+    public function setIsActive(bool $isActive): bool;
+
+    public function hasDuplicatedNames(string $name): bool;
+}
