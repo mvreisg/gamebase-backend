d4dadb959e4d63397e359c2a1a0e63455bd9dee4
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-04-19 17:22:22 -0300

    [update] on php cs fixer config file

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,125 +1,70 @@
 <?php
 
+use PhpCsFixer\ConfigInterface;
+
 $finder = \PhpCsFixer\Finder::create()
     ->in(__DIR__)
     ->exclude(['bootstrap', 'storage', 'vendor'])
-    ->name('*.php')
-    ->ignoreDotFiles(true)
-    ->ignoreVCS(true);
+    ->ignoreDotFiles(false)
+    ->name('*.php');
 
 return (new PhpCsFixer\Config())
+    ->setLineEnding("\n")
     ->setRules([
         '@PSR12' => true,
-        'array_indentation' => true,
+        'no_mixed_echo_print' => [
+            'use' => 'print'
+        ],
         'array_syntax' => [
             'syntax' => 'short'
-        ],        
-        'binary_operator_spaces' => [
-            'operators' => [
-                '=>' => 'align',
-                '=' => 'align'
-            ]
-        ],
-        'blank_line_after_opening_tag' => false,
-        'blank_lines_before_namespace' => false,
-        'blank_line_before_statement' => true,
-        'braces' => [
-            'allow_single_line_closure' => true,
-        ],
-        'cast_spaces' => true,
-        'class_definition' => [
-            'singleLine' => true
-        ],
-        'class_attributes_separation' => [
-            'elements' => [
-                'method' => 'one', 
-            ]
-        ],
-        'combine_consecutive_unsets' => true,
-        'concat_space' => [
-            'spacing' => 'one'
-        ],
-        'declare_equal_normalize' => true,
-        'function_typehint_space' => true,        
-        'include' => true,
-        'lowercase_cast' => true,
-        'multiline_whitespace_before_semicolons' => false,        
-        'native_function_casing' => true,
-        'new_with_braces' => true,
-        'no_blank_lines_after_class_opening' => true,
-        'no_blank_lines_after_phpdoc' => false,
-        'no_empty_comment' => true,
-        'no_empty_phpdoc' => true,
-        'no_empty_statement' => true,
-        'no_extra_blank_lines' => [
-            'tokens' => [
-                'curly_brace_block',
-                'extra',
-                'parenthesis_brace_block',
-                'square_brace_block',
-                'throw',
-                'use',
-            ]
-        ],
-        'no_leading_import_slash' => true,
-        'no_leading_namespace_whitespace' => true,
-        'no_mixed_echo_print' => [
-            'use' => 'echo'
         ],
         'no_multiline_whitespace_around_double_arrow' => true,
-        'no_short_bool_cast' => true,
-        'no_singleline_whitespace_before_semicolons' => true,
-        'no_spaces_around_offset' => true,
-        'no_trailing_comma_in_list_call' => true,
-        'no_trailing_comma_in_singleline_array' => true,
-        'no_unneeded_control_parentheses' => true,
-        'no_unused_imports' => true,
         'no_whitespace_before_comma_in_array' => true,
-        'no_whitespace_in_blank_line' => true,
-        'normalize_index_brace' => true,
-        'object_operator_without_whitespace' => true,
-        'php_unit_fqcn_annotation' => true,
-        'phpdoc_align' => true,
-        'phpdoc_annotation_without_dot' => true,
-        'phpdoc_indent' => true,
-        'phpdoc_inline_tag' => true,
-        'phpdoc_no_access' => true,
-        'phpdoc_no_alias_tag' => true,
-        'phpdoc_no_empty_return' => true,
-        'phpdoc_no_package' => true,
-        'phpdoc_no_useless_inheritdoc' => true,
-        'phpdoc_return_self_reference' => true,
-        'phpdoc_scalar' => true,
-        'phpdoc_separation' => true,
-        'phpdoc_single_line_var_spacing' => true,
-        'phpdoc_summary' => true,
-        'phpdoc_to_comment' => true,
-        'phpdoc_trim' => true,
-        'phpdoc_types' => true,
-        'phpdoc_var_without_name' => true,
-        'increment_style' => true,
-        'return_type_declaration' => true,
-        'self_accessor' => true,
-        'short_scalar_cast' => true,
-        'single_blank_line_at_eof' => true,
-        'single_blank_line_before_namespace' => true,        
-        'single_class_element_per_statement' => true,        
-        'single_line_comment_style' => [
-            'comment_types' => [
-                'hash'
-            ]
-        ],
-        'single_quote' => true,
-        'space_after_semicolon' => true,        
-        'standardize_not_equals' => true,
-        'ternary_operator_spaces' => true,
-        'trailing_comma_in_multiline' => [
-            'elements' => [
-                'arrays'
-            ]
-        ],
+        'return_to_yield_from' => true,
         'trim_array_spaces' => true,
-        'unary_operator_spaces' => true,
-        'whitespace_after_comma_in_array' => true,                
+        'whitespace_after_comma_in_array' => [
+            'ensure_single_space' => true
+        ],
+        'braces_position' => [
+            'allow_single_line_anonymous_functions' => false,
+            'allow_single_line_empty_anonymous_classes' => false,
+            'anonymous_classes_opening_brace' => 'next_line_unless_newline_at_signature_end',
+            'anonymous_functions_opening_brace' => 'next_line_unless_newline_at_signature_end',
+            'classes_opening_brace' => 'next_line_unless_newline_at_signature_end',
+            'control_structures_opening_brace' => 'next_line_unless_newline_at_signature_end',
+            'functions_opening_brace' => 'next_line_unless_newline_at_signature_end',
+        ],
+        'no_trailing_comma_in_singleline' =>  [
+            'elements' => [
+                'arguments',
+                'array',
+                'array_destructuring',
+                'group_import'
+            ]
+        ],
+        'single_line_empty_body' => true,
+        'class_reference_name_casing' => true,
+        'constant_case' => [
+            'case' => 'lower'
+        ],
+        'integer_literal_case' => true,
+        'lowercase_keywords' => true,
+        'lowercase_static_reference' => true,
+        'magic_constant_casing' => true,
+        'magic_method_casing' => true,
+        'native_function_casing' => true,
+        'native_type_declaration_casing' => true,
+        'cast_spaces' => [
+            'space' => 'none'
+        ],
+        'lowercase_cast' => true,
+        'no_short_bool_cast' => true,
+        'line_ending' => true,
+        'array_indentation' => true,
+        'indentation_type' => true,
+        'method_chaining_indentation' => true,
+        'statement_indentation' => [
+            'stick_comment_to_next_continuous_control_statement' => false
+        ]
     ])
     ->setFinder($finder);
