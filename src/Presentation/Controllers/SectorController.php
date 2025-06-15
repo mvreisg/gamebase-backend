bac5ec24a9e13d04d7fbc1388ee442df1a8518bb
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-06-15 15:06:55 -0300

    [feature] SectorController added

---
+++ src/Presentation/Controllers/SectorController.php
@@ -0,0 +1,409 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mvreisg\GamebaseBackend\Presentation\Controllers;
+
+use Mvreisg\GamebaseBackend\Application\Exceptions\AuthenticationException;
+use Mvreisg\GamebaseBackend\Application\Services\AuthenticationService;
+use Mvreisg\GamebaseBackend\Application\Services\SectorService;
+use Mvreisg\GamebaseBackend\Domain\Exceptions\EntityInvalidValueException;
+use Mvreisg\GamebaseBackend\Infrastructure\Exceptions\DatabaseDuplicatedEntryException;
+use Mvreisg\GamebaseBackend\Infrastructure\Exceptions\DatabaseFetchFailureException;
+use Mvreisg\GamebaseBackend\Infrastructure\Exceptions\DatabaseStatementCreationFailureException;
+use Mvreisg\GamebaseBackend\Infrastructure\Exceptions\DatabaseStatementExecutionFailureException;
+use Mvreisg\GamebaseBackend\Infrastructure\Exceptions\HttpJsonParseException;
+use Mvreisg\GamebaseBackend\Infrastructure\Http\HttpRequest;
+use Mvreisg\GamebaseBackend\Infrastructure\Http\HttpResponse;
+use Mvreisg\GamebaseBackend\Infrastructure\Http\HttpRouter;
+use Mvreisg\GamebaseBackend\Infrastructure\Middlewares\AuthorizationValidator;
+use Mvreisg\GamebaseBackend\Presentation\Exceptions\ControllerUndefinedValueException;
+use PDOException;
+
+class SectorController
+{
+    private SectorService $sectorService;
+    private AuthenticationService $authenticationService;
+
+    public function __construct(
+        SectorService $sectorService,
+        AuthenticationService $authenticationService
+    ) {
+        $this->sectorService = $sectorService;
+        $this->authenticationService = $authenticationService;
+    }
+
+    public function insert(HttpRequest $request, HttpResponse $response): void
+    {
+        try {
+            $headers = $request->getHeaders();
+            AuthorizationValidator::make()
+                ->setToken($headers)
+                ->validate($this->authenticationService);
+
+            $body = $request->parseBodyFromJSONString();
+
+            $isNameFieldSetted = isset($body['name']);
+            if ($isNameFieldSetted === false) {
+                throw new ControllerUndefinedValueException('A chave name não existe ou seu valor é null');
+            }
+
+            $isIsActiveFieldSetted = isset($body['isActive']);
+            if ($isIsActiveFieldSetted === false) {
+                throw new ControllerUndefinedValueException('A chave isActive não existe ou seu valor é null');
+            }
+
+            $name = $body['name'];
+            $isActive = $body['isActive'];
+
+            $sector = $this->sectorService->insert($name, $isActive);
+
+            $response
+                ->setBody([
+                    'message' => 'Setor inserido com sucesso!',
+                    'data' => [
+                        'id' => $sector->getId(),
+                        'name' => $sector->getName(),
+                        'isActive' => $sector->getIsActive()
+                    ]
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[201])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (AuthenticationException $e) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[401])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (
+            ControllerUndefinedValueException |
+            HttpJsonParseException |
+            EntityInvalidValueException |
+            DatabaseDuplicatedEntryException $e
+        ) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[400])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (
+            DatabaseStatementCreationFailureException |
+            DatabaseStatementExecutionFailureException |
+            DatabaseFetchFailureException |
+            PDOException $e
+        ) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[500])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        }
+    }
+
+    public function update(HttpRequest $request, HttpResponse $response): void
+    {
+        try {
+            $headers = $request->getHeaders();
+            AuthorizationValidator::make()
+                ->setToken($headers)
+                ->validate($this->authenticationService);
+
+            $body = $request->parseBodyFromJSONString();
+            $params = $request->getParams();
+
+            $isIdSetted = isset($params['id']);
+            if ($isIdSetted === false) {
+                throw new ControllerUndefinedValueException(
+                    'O parâmetro id não foi informado na URL ou seu valor é null!'
+                );
+            }
+
+            $isNameFieldSetted = isset($body['name']);
+            if ($isNameFieldSetted === false) {
+                throw new ControllerUndefinedValueException('A chave name não foi informada ou seu valor é null!');
+            }
+
+            $isIsActiveFieldSetted = isset($body['isActive']);
+            if ($isIsActiveFieldSetted === false) {
+                throw new ControllerUndefinedValueException('A chave isActive não foi informada ou seu valor é null!');
+            }
+
+            $id = $params['id'];
+            $name = $body['name'];
+            $isActive = $body['isActive'];
+
+            $wasAUpdateOcurred = $this->sectorService->update($id, $name, $isActive);
+            if ($wasAUpdateOcurred === false) {
+                $response
+                    ->setBody([
+                        'message' => 'Nenhuma linha afetada.'
+                    ])
+                    ->setStatus(HttpRouter::$STATUS_CODES[200])
+                    ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+                return;
+            }
+
+            $response
+                ->setBody([
+                    'message' => 'Setor atualizado com sucesso!'
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[200])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (AuthenticationException $e) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[401])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (
+            ControllerUndefinedValueException |
+            HttpJsonParseException |
+            EntityInvalidValueException |
+            DatabaseDuplicatedEntryException $e
+        ) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[400])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (
+            DatabaseStatementCreationFailureException |
+            DatabaseStatementExecutionFailureException |
+            PDOException $e
+        ) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[500])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        }
+    }
+
+    public function setIsActive(HttpRequest $request, HttpResponse $response): void
+    {
+        try {
+            $headers = $request->getHeaders();
+            AuthorizationValidator::make()
+                ->setToken($headers)
+                ->validate($this->authenticationService);
+
+            $params = $request->getParams();
+            $body = $request->parseBodyFromJSONString();
+
+            $isIdSetted = isset($params['id']);
+            if ($isIdSetted === false) {
+                throw new ControllerUndefinedValueException('O parâmetro id não foi informado na URL!');
+            }
+
+            $isIsActiveFieldSetted = isset($body['isActive']);
+            if ($isIsActiveFieldSetted === false) {
+                throw new ControllerUndefinedValueException('A chave isActive não existe ou seu valor é null!');
+            }
+
+            $id = $params['id'];
+            $isActive = $body['isActive'];
+
+            $wasTheUpdateOcurred = $this->sectorService->setIsActive($id, $isActive);
+            if ($wasTheUpdateOcurred === false) {
+                $response
+                    ->setBody([
+                        'message' => 'Nenhum registro foi modificado!'
+                    ])
+                    ->setStatus(HttpRouter::$STATUS_CODES[200])
+                    ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+                return;
+            }
+
+            $response
+                ->setBody([
+                    'message' => 'Estado atualizado com sucesso!'
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[200])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (AuthenticationException $e) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[401])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (
+            ControllerUndefinedValueException |
+            HttpJsonParseException |
+            EntityInvalidValueException $e
+        ) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[400])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (
+            DatabaseStatementCreationFailureException |
+            DatabaseStatementExecutionFailureException |
+            PDOException $e
+        ) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[500])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        }
+    }
+
+    public function findById(HttpRequest $request, HttpResponse $response): void
+    {
+        try {
+            $headers = $request->getHeaders();
+            AuthorizationValidator::make()
+                ->setToken($headers)
+                ->validate($this->authenticationService);
+
+            $params = $request->getParams();
+
+            $isIdSetted = isset($params['id']);
+            if ($isIdSetted === false) {
+                throw new ControllerUndefinedValueException(
+                    'O parâmetro id não foi informado ou seu valor é null!'
+                );
+            }
+
+            $id = $params['id'];
+
+            $sector = $this->sectorService->findById($id);
+
+            if ($sector === null) {
+                $response
+                    ->setBody([
+                        'message' => 'O setor com o id ' . $id . ' não existe!',
+                    ])
+                    ->setStatus(HttpRouter::$STATUS_CODES[200])
+                    ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+                return;
+            }
+
+            $response
+                ->setBody([
+                    'message' => 'Setor encontrado com sucesso!',
+                    'data' => [
+                        'id' => $sector->getId(),
+                        'name' => $sector->getName(),
+                        'isActive' => $sector->getIsActive()
+                    ]
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[200])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (AuthenticationException $e) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[401])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (
+            ControllerUndefinedValueException |
+            EntityInvalidValueException $e
+        ) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[400])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (
+            DatabaseStatementCreationFailureException |
+            DatabaseStatementExecutionFailureException |
+            PDOException $e
+        ) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[500])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        }
+    }
+
+    public function findAll(HttpRequest $request, HttpResponse $response): void
+    {
+        try {
+            $headers = $request->getHeaders();
+            AuthorizationValidator::make()
+                ->setToken($headers)
+                ->validate($this->authenticationService);
+
+            $sectors = $this->sectorService->findAll();
+
+            $numberOfGenresFound = count($sectors);
+            if ($numberOfGenresFound === 0) {
+                $response
+                    ->setBody([
+                        'message' => 'Nenhum registro encontrado!',
+                    ])
+                    ->setStatus(HttpRouter::$STATUS_CODES[200])
+                    ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+                return;
+            }
+
+            foreach ($sectors as $sector) {
+                $data[] = [
+                    'id' => $sector->getId(),
+                    'name' => $sector->getName(),
+                    'isActive' => $sector->getIsActive()
+                ];
+            }
+
+            $response
+                ->setBody([
+                    'message' => 'Permissões buscados com sucesso!',
+                    'data' => $data
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[200])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (AuthenticationException $e) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[401])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        } catch (
+            DatabaseStatementCreationFailureException |
+            DatabaseStatementExecutionFailureException |
+            PDOException $e
+        ) {
+            $response
+                ->setBody([
+                    'message' => $e->getMessage()
+                ])
+                ->setStatus(HttpRouter::$STATUS_CODES[500])
+                ->send(HttpRouter::$CONTENT_TYPES['JSON']);
+            return;
+        }
+    }
+}
