5a26e6fa4d358364e02dff3333748993efe17355
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2026-05-18 16:56:11 -0300

    3.0.0-preview-6.2.1 (checkpoint commit) - feat: documentation for http authentication controller routes

--- src/Presentation/Http/Controller/HttpAuthenticationController.php
+++ src/Presentation/Http/Controller/HttpAuthenticationController.php
@@ -29,12 +29,20 @@ class HttpAuthenticationController
         summary: "Validate",
         description:
             "Validates the provided authentication token and returns the associated authentication data if valid.",
-        tags: ["Authentication"],
-        security: [["bearerAuth" => []]],
+        tags: ["Authentication"]
+    )]
+    #[OA\Parameter(
+        name: "Authorization",
+        in: "header",
+        required: true,
+        description: "Bearer token",
+        schema: new OA\Schema(
+            type: "string"
+        )
     )]
     #[OA\Response(
         response: 200,
-        description: "Valid token",
+        description: "Response if token is valid",
         content: new OA\JsonContent(
             properties: [
                 new OA\Property(
@@ -46,6 +54,65 @@ class HttpAuthenticationController
                     property: "data",
                     type: "object",
                     ref: "#/components/schemas/AuthenticationData"
+                )
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Response if user does not have credentials",
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    property: "message",
+                    type: "string",
+                ),
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 404,
+        description: "Response if the user does not exist",
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    property: "message",
+                    type: "string",
+                )
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 500,
+        description: "Response if a internal server error occurs",
+        content: new OA\JsonContent(
+            oneOf: [
+                new OA\Schema(
+                    title: "Encryption error",
+                    properties: [
+                        new OA\Property(
+                            property: "message",
+                            type: "string",
+                        ),
+                    ]
+                ),
+                new OA\Schema(
+                    title: "Authentication token cache exception",
+                    properties: [
+                        new OA\Property(
+                            property: "message",
+                            type: "string",
+                        )
+                    ]
+                ),
+                new OA\Schema(
+                    title: "Authentication token provider exception",
+                    properties: [
+                        new OA\Property(
+                            property: "message",
+                            type: "string",
+                        )
+                    ]
                 )
             ]
         )
