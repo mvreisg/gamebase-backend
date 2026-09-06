e0328b66df3aff0d22a8d7b808ab4563795680c0
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2026-09-06 10:55:39 -0300

    fix: add timestamp to CheckAuthorizationUseCase logs

--- src/Application/Authorization/UseCase/CheckAuthorizationUseCase.php
+++ src/Application/Authorization/UseCase/CheckAuthorizationUseCase.php
@@ -8,6 +8,7 @@ use Mvreisg\GamebaseBackend\Domain\Authentication\Service\AuthenticationService;
 use Mvreisg\GamebaseBackend\Domain\Authorization\Permission\PermissionType;
 use Mvreisg\GamebaseBackend\Domain\Authorization\Sector\SectorType;
 use Mvreisg\GamebaseBackend\Domain\Authorization\Service\AuthorizationDomainService;
+use Mvreisg\GamebaseBackend\Domain\Shared\Interface\ClockInterface;
 use Mvreisg\GamebaseBackend\Domain\User\Service\UserDomainService;
 use Mvreisg\GamebaseBackend\Domain\UserSectorPermission\Entity\Collection\UserSectorPermissionCollection;
 use Mvreisg\GamebaseBackend\Domain\UserSectorPermission\Repository\UserSectorPermissionRepositoryInterface;
@@ -19,6 +20,7 @@ class CheckAuthorizationUseCase
     private UserSectorPermissionRepositoryInterface $userSectorPermissionRepository;
     private AuthenticationService $authenticationService;
     private AuthorizationDomainService $authorizationDomainService;
+    private ClockInterface $clock;
     private LoggerInterface $logger;
 
     public function __construct(
@@ -26,16 +28,18 @@ class CheckAuthorizationUseCase
         UserSectorPermissionRepositoryInterface $userSectorPermissionRepository,
         AuthenticationService $authenticationService,
         AuthorizationDomainService $authorizationDomainService,
+        ClockInterface $clock,
         LoggerInterface $logger
     ) {
         $this->userDomainService = $userDomainService;
         $this->userSectorPermissionRepository = $userSectorPermissionRepository;
         $this->authenticationService = $authenticationService;
         $this->authorizationDomainService = $authorizationDomainService;
+        $this->clock = $clock;
         $this->logger = $logger;
     }
 
-    public function execute(string $token, SectorType $sectorType, PermissionType $permissionType): void
+    public function execute(string $token, SectorType $sectorType, PermissionType $permissionType): bool
     {
         try {
             $decodedToken = $this->authenticationService->validate(
@@ -61,11 +65,20 @@ class CheckAuthorizationUseCase
                 $sectorType,
                 $permissionType
             );
+
+            $this->logger->error("Successfully authorized", [
+                "sector" => $sectorType->value,
+                "permission" => $permissionType->value,
+                "timestamp" => $this->clock->now()->format(\DateTimeInterface::ATOM)
+            ]);
+
+            return true;
         } catch (\Throwable $e) {
             $this->logger->error("Authorization check failed", [
-                "sectorType" => $sectorType->value,
-                "permissionType" => $permissionType->value,
-                "error" => $e->getMessage(),
+                "sector" => $sectorType->value,
+                "permission" => $permissionType->value,
+                "exception" => $e->getMessage(),
+                "timestamp" => $this->clock->now()->format(\DateTimeInterface::ATOM)
             ]);
             throw $e;
         }
